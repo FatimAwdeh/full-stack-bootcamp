a684b4fa714b764086664f76ac3f56158e79db04
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-08-17 12:45:26 +0300

    Challenge 1 - Done with Step 5

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,9 +1,41 @@
-//console.log('I am linked now!');
-/*let name="Fatima";
-alert(name);*/
+console.log('I am linked now!');
+let name="Fatima";
+alert(name);
 let age=19;
 const lastName="Awdeh";
 var DOB="21/8/2004";
 console.log(age);
 console.log(lastName);
 console.log(DOB);
+const string="This is a text";
+console.log(string);
+const bool=true;
+console.log(bool);
+const bool1=false;
+console.log(bool1);
+const nb=1215;
+console.log(nb);
+const nb1="999";
+console.log(nb1);
+var myArray=[];
+console.log(myArray[0]);//donne undefined 
+let str;
+console.log(str);//donne undefined 
+let objet = null;
+console.log(objet); 
+let myText1="Enter the text 1";
+console.log(myText1);
+let myText2='Enter the text 2 ';
+console.log(myText2);
+let myText3=`Enter the text 3`;
+console.log(myText3);
+const counter = 3;
+const backtick = `This is my text using backticks ${counter} times`;
+const singleQuote ='This is my text using single quotations ${counter} times';
+const doubleQuote ="This is my text using double quotations ${counter} times";
+const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+console.log(counter);
+console.log(backtick);//prend une variable et donne la valeur du counter 
+console.log(singleQuote);//donne la str comme elle est
+console.log(doubleQuote);//donne la str comme elle est
+console.log(tryThisAlso);//faire la calculation d'une variable d'expression
