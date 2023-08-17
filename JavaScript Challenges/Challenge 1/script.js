706d7ac6f173ad73e9a445ff97a9bb31ef22ee32
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-08-17 11:16:26 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,3 +1,9 @@
 //console.log('I am linked now!');
-let name="Fatima";
-alert(name);
+/*let name="Fatima";
+alert(name);*/
+let age=19;
+const lastName="Awdeh";
+var DOB="21/8/2004";
+console.log(age);
+console.log(lastName);
+console.log(DOB);
