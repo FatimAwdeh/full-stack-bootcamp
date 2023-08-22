26837882d70acfb152464f88a97cf8de943d16aa
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-08-22 14:55:16 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,15 @@
+let fiveNumbers=[1,2,54,766,321];
+console.log(fiveNumbers);
+
+
+const personalInfo = {
+    name: "Fatima Awdeh",
+    age: 19,
+    height: 155,
+    location: "Baalback",
+    nationality: "Libanaise"
+  };
+console.log(personalInfo);
+
+
+  
