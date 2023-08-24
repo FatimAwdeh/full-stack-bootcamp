4a079f9820d75e5c87f2b4ee7c62a2787a201b01
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-08-24 16:28:38 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -13,3 +13,29 @@ button.addEventListener('click', function() {
     const randomNumber = Math.floor(Math.random() * 1000) + 1;
     button.textContent = 'Clicked ' + randomNumber + '!';
   });
+
+  const h1=document.createElement('h1');
+  h1.textContent='h1 content';
+  document.body.appendChild(h1);
+  h1.style.border = '10px dotted green';
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'a' || event.key === 'A') {
+      h1.style.backgroundColor = 'red';
+    }
+    else if (event.key === 's' || event.key === 'S') {
+    h1.style.marginLeft = '10px';
+  }
+   else if (event.key === 'd' || event.key === 'D') {
+    const p = document.createElement('p');
+        p.textContent = 'Key D was pressed!';
+        h1.appendChild(p);
+    }
+    else if (event.key === 'w' || event.key === 'W') {
+       h1.style.display='none';
+        }
+    else if (event.key == ' ') {
+        h1.style.fontSize='50px';
+    }
+});
+
