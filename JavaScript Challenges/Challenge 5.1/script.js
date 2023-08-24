3b12ffd6d1effa6506bef596869f1047dcbb892e
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-08-24 17:01:15 +0300

    Challenge 5.1 - Done with step 3

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -1,3 +1,4 @@
+//step 1
 const button = document.createElement('button');
 button.textContent = 'Click Me';
 document.body.appendChild(button);
@@ -14,6 +15,9 @@ button.addEventListener('click', function() {
     button.textContent = 'Clicked ' + randomNumber + '!';
   });
 
+
+
+  //step 2
   const h1=document.createElement('h1');
   h1.textContent='h1 content';
   document.body.appendChild(h1);
@@ -39,3 +43,97 @@ button.addEventListener('click', function() {
     }
 });
 
+
+
+
+//step 3
+const form = document.createElement('form');
+
+const fullNameLabel = document.createElement('label');
+fullNameLabel.textContent = 'Full Name:';
+form.appendChild(fullNameLabel);
+
+const fullNameInput = document.createElement('input');
+fullNameInput.type = 'text';
+fullNameInput.name = 'fullname';
+form.appendChild(fullNameInput);
+form.appendChild(document.createElement('br')); 
+form.appendChild(document.createElement('br')); 
+
+const emailLabel = document.createElement('label');
+emailLabel.textContent = 'Email Address:';
+form.appendChild(emailLabel);
+
+const emailInput = document.createElement('input');
+emailInput.type = 'email';
+emailInput.name = 'email';
+form.appendChild(emailInput);
+form.appendChild(document.createElement('br')); 
+form.appendChild(document.createElement('br')); 
+
+const errorElement = document.createElement('p');
+errorElement.style.color = 'red';
+form.appendChild(errorElement);
+
+
+const passwordLabel = document.createElement('label');
+passwordLabel.textContent = 'Password:';
+form.appendChild(passwordLabel);
+
+const passwordInput = document.createElement('input');
+passwordInput.type = 'password';
+passwordInput.name = 'password';
+form.appendChild(passwordInput);
+form.appendChild(document.createElement('br')); 
+form.appendChild(document.createElement('br')); 
+
+const confirmPasswordLabel = document.createElement('label');
+confirmPasswordLabel.textContent = 'Confirm Password:';
+form.appendChild(confirmPasswordLabel);
+
+const confirmPasswordInput = document.createElement('input');
+confirmPasswordInput.type = 'password';
+confirmPasswordInput.name = 'confirmPassword';
+form.appendChild(confirmPasswordInput);
+form.appendChild(document.createElement('br')); 
+form.appendChild(document.createElement('br')); 
+
+const successElement = document.createElement('p');
+successElement.style.color = 'green';
+form.appendChild(successElement);
+
+const submitButton = document.createElement('input');
+submitButton.type = 'submit';
+submitButton.value = 'Submit';
+form.appendChild(submitButton);
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+})
+
+//valid email
+const email = emailInput.value.trim();
+  if (!isValidEmail(email)) {
+    errorElement.textContent = 'Please enter a valid email address.';
+    //return ;
+  }
+    errorElement.textContent = 'Form submitted successfully!';
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+//confirm password
+
+const password = passwordInput.value.trim();
+  const confirmPassword = confirmPasswordInput.value.trim();
+  if (password !== confirmPassword) {
+    errorElement.textContent = 'Passwords do not match.';
+   // return;
+  } errorElement.textContent = 'Form submitted successfully!';
+
+  errorElement.textContent = '';
+  successElement.textContent = 'Form submitted successfully!';
+
+
+document.body.appendChild(form);
